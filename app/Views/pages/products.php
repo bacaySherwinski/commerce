af78cfa741cb2df06592913c97373fd0fdee640e
Bacay, Sherwin <bacaylabog@gmail.com>
2023-09-29 19:42:07 +0800

    Update products.php

--- app/Views/pages/products.php
+++ app/Views/pages/products.php
@@ -1,7 +1,7 @@
 <?= $this->include('include/top') ?>
 <?= $this->include('include/header') ?>
- <?= $this->include('include/navbar') ?>
- <?= $this->include('include/end') ?>
+<?= $this->include('include/navbar') ?>
+<?= $this->include('include/end') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student List</title>
     <style>
-       
         body, table, .form-container {
             font-family: arial, sans-serif;
         }
@@ -17,7 +16,6 @@
             text-align: center;
         }
 
-        
         table {
             border-collapse: collapse;
             width: 90%;
@@ -42,7 +40,7 @@
             padding: 10px;
             margin-left: 20px;
             float: left;
-            width: 25%;
+            width: 100%;
         }
         .form-container label {
             display: block;
@@ -62,10 +60,56 @@
         .clear {
             clear: both;
         }
+
+        /* Define custom button styles */
+        .custom-button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #007BFF; /* Button background color */
+            color: #fff; /* Text color */
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s ease; /* Smooth hover transition */
+        }
+
+        .custom-button:hover {
+            background-color: #0056b3; /* Change background color on hover */
+        }
+
+        /* Modal styles */
+        .modal {
+    display: none;
+    position: fixed;
+    top: 1px;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 999; /* Adjust this value as needed */
+}
+
+
+        .modal-content {
+            background-color: #fff;
+            margin: 2% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 50%;
+            position: relative;
+        }
+
+        .close {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            font-size: 24px;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
-    
     <table>
         <tr>
             <th>Name</th>
@@ -83,11 +127,74 @@
                 <td><?= $pr['price'] ?></td>
                 <td><?= $pr['category'] ?></td>
                 <td><?= $pr['quantity'] ?></td>
-               
             </tr>
         <?php endforeach; ?>
     </table>
 
-   
+    <!-- Add Products Button with Bootstrap Styling -->
+    <button id="openModalBtn" class="custom-button">Add Products</button>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span id="closeModalBtn" class="close">&times;</span>
+            <!-- Your modal content here -->
+            <h2>Add Products</h2>
+            <!-- Add form fields or content for adding products here -->
+            <div class="form-container">
+        <form action="/save" method="post">
+            <label>Name</label>
+            <input type="hidden" name="id" value="<?= isset($stu) ? $stu['id'] : '' ?>">
+            <input type="text" name="name" placeholder="Name" value="<?= isset($stu) ? $stu['name'] : '' ?>">
+            <br>
+            <label>Address</label>
+            <input type="text" name="address" placeholder="Address" value="<?= isset($stu) ? $stu['address'] : '' ?>">
+            <br>
+            <label>Phone Number</label>
+            <input type="text" name="number" placeholder="Phone Number" value="<?= isset($stu) ? $stu['number'] : '' ?>">
+            <br>
+            <label>Sex</label>
+            <input type="text" name="sex" placeholder="Sex" value="<?= isset($stu) ? $stu['sex'] : '' ?>">
+            <br>
+            <label>Id Number</label>
+            <input type="text" name="studentId" placeholder="Id Number" value="<?= isset($stu) ? $stu['studentId'] : '' ?>">
+            <br>
+            <div style="margin-top: 10px;"></div>
+            <input type="submit" value="Save">
+        </form>
+    </div>
+        </div>
+    </div>
+
+    <!-- JavaScript to handle the modal -->
+    <script>
+        // Get the modal and buttons
+        var openModalBtn = document.getElementById("openModalBtn");
+        var modal = document.getElementById("myModal");
+        var closeModalBtn = document.getElementById("closeModalBtn");
+
+        // Function to open the modal
+        function openModal() {
+            modal.style.display = "block";
+        }
+
+        // Function to close the modal
+        function closeModal() {
+            modal.style.display = "none";
+        }
+
+        // Event listener for opening the modal when the button is clicked
+        openModalBtn.addEventListener("click", openModal);
+
+        // Event listener for closing the modal when the close button is clicked
+        closeModalBtn.addEventListener("click", closeModal);
+
+        // Event listener for closing the modal when clicking outside the modal
+        window.addEventListener("click", function(event) {
+            if (event.target == modal) {
+                closeModal();
+            }
+        });
+    </script>
 </body>
 </html>
